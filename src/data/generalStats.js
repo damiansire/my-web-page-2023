a7649151a9e3a18aa3805d67e96ed83de19aa4f5
Damian Sire <damiansire@hotmail.com>
2023-02-07 02:15:42 -0300

    feat(css): add new styles

---
+++ src/data/generalStats.js
@@ -0,0 +1,6 @@
+export const generalStats = [
+    { iconName: "bi:facebook", title: "Subscriptores", text: "28.000" },
+    { iconName: "mdi:youtube", title: "Visitas en youtube", text: "1.300.000" },
+    { iconName: "fa:meetup", title: "Charlas en Eventos", text: "31" },
+    { iconName: "mdi:clock-time-four-outline", title: "Watch Time", text: "+70.000h" }
+]
