bc0c079cb93f04dd5bd14b29ef27eca4c7b037a6
Damian Sire <damiansire@hotmail.com>
2023-02-08 00:54:10 -0300

    feat(styles): add new component for badge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'http://localhost:3000',
+  integrations: [tailwind(), sitemap({
+    i18n: {
+      defaultLocale: 'es',
+      // All urls that don't contain `en` after `http://localhost:3000/` will be treated as default locale, i.e. `es`
+      locales: {
+        es: 'es-ES', // The `defaultLocale` value must present in `locales` keys
+        en: 'en-US',
+      }
+    }
+  })]
 });
---
+++ src/data/talksData.js
@@ -0,0 +1,267 @@
+export const talksData = [
+    {
+        communityName: "DevFest Mexico",
+        talkLink: "https://devfestmexico.com/speakers/",
+        talkTitle: "Inteligencia Artificial con JavaScript",
+        country: "Mexico",
+        communityImg: "/communityImages/DevFestMexico.png",
+        year: "2021",
+    },
+    {
+        communityName: "AngularMVD",
+        talkLink: "https://www.meetup.com/es/Angular-MVD/events/fxpjxryccpbwb/",
+        talkTitle: "De 0 a 100 en TensorFlowJs con Angular",
+        country: "Uruguay",
+        communityImg: "/communityImages/AngularMVD.jpg",
+        year: "2021",
+    },
+    {
+        communityName: "MacaoTech",
+        talkLink: "https://www.facebook.com/watch/live/?ref=watch_permalink&v=577529729997083",
+        talkTitle: "Youtube Api + Twitter Api con Node",
+        country: "Latam",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AAUvwngMTjskcCIWREudm7iphXTI0jz4QMlBhQTxgVDbTQ=s176-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "Feria de Empleo Ort",
+        talkLink: "https://www.youtube.com/watch?v=aix07a2mpF0",
+        talkTitle: "La historia perdida de Json",
+        country: "Uruguay",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AKedOLQzFW43IJKnDXoa36jRewLnq5V6nhZb1efZKdN7=s48-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "Python Cochabamba",
+        talkLink:
+            "https://www.youtube.com/watch?v=FZagdt0Ptmg",
+        talkTitle: "",
+        country: "Bolivia",
+        communityImg: "https://yt3.ggpht.com/ytc/AKedOLSguLnwffZAjmikbaX8YNUbeYrs4DA2si5k9Uo=s88-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "Javascript Montevideo",
+        talkLink: "https://www.meetup.com/montevideojs/events/281150261/",
+        talkTitle: "Programacion Visual con p5.js",
+        country: "Uruguay",
+        communityImg:
+            "https://pbs.twimg.com/profile_images/706887943256973312/KScAgsOs_400x400.jpg",
+        year: "2021",
+    },
+    {
+        communityName: "Angular Bolivia",
+        talkLink:
+            "https://www.youtube.com/watch?v=FZagdt0Ptmg",
+        talkTitle: "",
+        country: "Bolivia",
+        communityImg: "https://yt3.ggpht.com/ytc/AKedOLSguLnwffZAjmikbaX8YNUbeYrs4DA2si5k9Uo=s88-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "Angular Dominicana",
+        talkLink:
+            "https://www.youtube.com/watch?v=yNwGG1pFiC0",
+        talkTitle: "Inyección de dependencias por dentro en Angular",
+        country: "Republica Dominicana",
+        communityImg: "https://pbs.twimg.com/profile_images/1137941511415484416/0MhpUErP_400x400.png",
+        year: "2021",
+    },
+    {
+        communityName: "Angular Bogotá",
+        talkLink:
+            "https://www.youtube.com/watch?v=RyiojuRLcAI&ab_channel=AngularBogot%C3%A1",
+        talkTitle: "De 0 a 100 en Inteligencia Artificial con Angular",
+        country: "Colombia",
+        communityImg: "/communityImages/AngularBogota.jpg",
+        year: "2021",
+    },
+    {
+        communityName: "Angula Honduras",
+        talkLink:
+            "https://www.youtube.com/watch?v=uVLE8E6PAJs&t=295s&ab_channel=AngularHonduras",
+        talkTitle: "Modelos clasicos de IA en Angular",
+        country: "Honduras",
+        communityImg: "/communityImages/AngularHonduras.jpg",
+        year: "2021",
+    },
+    {
+        communityName: "NodeColombia",
+        talkLink: "https://www.meetup.com/es/node_co/events/277160045/",
+        talkTitle: "Interaccion con el usuario mediante IA - Tensorflow.js",
+        country: "Colombia",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AAUvwngsMHvHHY9e9p59x6zUBssUvvsjmi0eMarud6Hn=s176-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "NgRosario",
+        talkLink: "https://www.meetup.com/es/Ng-Rosario/events/277094219/",
+        talkTitle:
+            "Detectando si una persona usa tapa-boca mediante IA - Angular + TFJS",
+        country: "Argentina",
+        communityImg:
+            "https://i.imgur.com/v8rmqr9_d.webp?maxwidth=760&fidelity=grand",
+        year: "2021",
+    },
+    {
+        communityName: "Python Day Nicaragua",
+        talkLink: "https://www.meetup.com/es/Python-Nicaragua/events/277569563/",
+        talkTitle: "Redes Neuronales con python - Keras + Tensorflow",
+        country: "Nicaragua",
+        communityImg: "https://i.imgur.com/6Dx2PhH.png",
+        year: "2021",
+    },
+    {
+        communityName: "MacaoTech",
+        talkLink: "https://www.youtube.com/watch?v=D2DfD74-77E&t=1s",
+        talkTitle: "Cms headless y la eleccion",
+        country: "Latam",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AAUvwngMTjskcCIWREudm7iphXTI0jz4QMlBhQTxgVDbTQ=s176-c-k-c0x00ffffff-no-rj",
+        year: "2021",
+    },
+    {
+        communityName: "PyLadies UY",
+        talkLink: "https://www.meetup.com/es/PyLadiesUy/events/269044767/",
+        talkTitle: "Persiguiendo al COVID-19 por Damián Siré",
+        country: "Uruguay",
+        communityImg: "/communityImages/PyLadiesUy.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "Python Argentina",
+        talkLink:
+            "https://www.meetup.com/es/Buenos-Aires-Python-Meetup/events/271011152/",
+        talkTitle: "Introduccion a Numpy y Pandas por Damián Siré",
+        country: "Argentina",
+        communityImg:
+            "https://pbs.twimg.com/profile_images/1271192632971141120/rD4ykoBS_400x400.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "Matea Social",
+        talkLink: "https: //matea.social/academia.html",
+        talkTitle: "Angular Vs React ¿Que los diferencia?",
+        country: "Uruguay",
+        communityImg:
+            "https://avatars.githubusercontent.com/u/59933886?s=200&v=4",
+        year: "2020",
+    },
+    {
+        communityName: "AngularMVD",
+        talkLink: "https://www.meetup.com/es/Angular-MVD/events/fxpjxryccgbcc/",
+        talkTitle: "Controlando nuestra app con IA",
+        country: "Uruguay",
+        communityImg: "/communityImages/AngularMVD.jpg",
+        year: "2021",
+    },
+    {
+        communityName: "Javascript Montevideo",
+        talkLink: "https://www.meetup.com/es/montevideojs/events/271022594/",
+        talkTitle: "RxJs - Observables y Operadores",
+        country: "Uruguay",
+        communityImg:
+            "https://pbs.twimg.com/profile_images/706887943256973312/KScAgsOs_400x400.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "NgPanama",
+        talkLink: "https://www.meetup.com/es/Ng-Panama/events/271661906/",
+        talkTitle: "Programacion Reactiva en Angular con RxJs y Observables",
+        country: "Panama",
+        communityImg:
+            "https://angular.lat/assets/meetups/ngPanama.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "Python Pereira - Colombia",
+        talkLink: "https://www.meetup.com/es/pythonpereira/events/272743156/",
+        talkTitle: "Inteligencia artificial con Python",
+        country: "Colombia",
+        communityImg: "https://pypereira.co/static/images/2019.svg",
+        year: "2020",
+    },
+    {
+        communityName: "Joacos Twitters Spaces",
+        talkLink: "https://twitter.com/JoaoPixeles",
+        talkTitle: "Estructura de datos y Algoritmos",
+        country: "Colombia",
+        communityImg: "https://i.imgur.com/DBQTBY7_d.webp?maxwidth=760",
+        year: "2021",
+    },
+    {
+        communityName: "Nube Colectiva",
+        talkLink: "https://www.youtube.com/watch?v=J69MbeM6b8U&t=5s&ab_channel=NubeColectiva",
+        talkTitle: "Novedades de Angular 12",
+        country: "Peru",
+        communityImg: "/communityImages/NubeColectiva.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "AngularMVD",
+        talkLink: "https://www.meetup.com/es/Angular-MVD/events/ktqljlybchbbc/",
+        talkTitle: "Visualización de datos en angular con PrimeNg",
+        country: "Uruguay",
+        communityImg: "/communityImages/AngularMVD.jpg",
+        year: "2020",
+    },
+    {
+        communityName: "Manuel - UndefinedDevs",
+        talkLink: "https://www.youtube.com/watch?v=tiFQRqkZ6aU",
+        talkTitle: "La evolucion de las comunidades tecnologicas",
+        country: "Colombia",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AAUvwngJW-BHmQ9XpatLhyRwdZQytBVhbVxkgRn_0AKolA=s88-c-k-c0x00ffffff-no-rj",
+        year: "2020",
+    },
+    {
+        communityName: "Dominicode",
+        talkLink: "https://www.youtube.com/watch?v=56535Sf6sgE",
+        talkTitle: "Inteligencia artificial en Angular con Damian Sire",
+        country: "Republica Dominicana",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AAUvwniISbBEa0hK5xdcnrmRDHrXF3UEidXXW1QjsDuL=s900-c-k-c0x00ffffff-no-rj",
+        year: "2020",
+    },
+    {
+        communityName: "Devteam504",
+        talkLink: " https://www.youtube.com/watch?v=TaI9eqRgqMI",
+        talkTitle: "Machine Learning Visual en Javascript con P5Js y Ml5",
+        country: "Honduras",
+        communityImg:
+            "https://yt3.ggpht.com/ytc/AKedOLSrqgZVSQg0YS5cmhumH7zwE6NCDDO7DQmcqS-u9A=s48-c-k-c0x00ffffff-no-rj",
+        year: "2020",
+    },
+    {
+        communityName: "Python Bolivia",
+        talkLink: "https://www.facebook.com/pyconbolivia/posts/768130157334550",
+        talkTitle: "Introducción al machine learning",
+        country: "Bolivia",
+        communityImg: "/communityImages/PythonBolivia.png",
+        year: "2020",
+    },
+    {
+        communityName: "JavascriptMontevideo",
+        talkLink: "https://www.meetup.com/es/montevideojs/events/264156360/",
+        talkTitle: "Programación Dinámica + ML con JS por Damián Siré",
+        country: "Uruguay",
+        communityImg:
+            "https://pbs.twimg.com/profile_images/706887943256973312/KScAgsOs_400x400.jpg",
+        year: "2020",
+    },
+]
+
+export const talkForCountry = talksData.reduce((acc, curr) => {
+    if (acc[curr.country]) {
+        acc[curr.country].push(curr)
+    }
+    else {
+        acc[curr.country] = [curr]
+    }
+    return acc;
+}, {})
+
+export const lenght = talksData.length
