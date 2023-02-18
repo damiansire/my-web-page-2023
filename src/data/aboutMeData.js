9696442bc202f2159be1e62e350b48aa93d6a489
Damian Sire <damiansire@hotmail.com>
2023-02-18 01:08:51 -0300

    feat(aboutme): get aboutMe data from js

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,32 @@
+export const aboutMeData = [
+    {
+        number: "01.",
+        title: "Welcome my life :)",
+        text: "My name is Damian Sire and in the following lines I will try to tell you who I am and what I do.",
+    },
+    {
+        number: "02.",
+        title: "Way of life",
+        text: "I love numbers! I collect data and insights, analyze them then take the time to learn about their, ask me the right questions to understand the things."
+    },
+    {
+        number: "03.",
+        title: "What do I like?",
+        text: "My biggest passions are programming, automation and data visualization. I love learning and if what I learn is related to programming, I like it even more."
+    },
+    {
+        number: "04.",
+        title: "The community",
+        text: "I have been actively attending meetups for years, either in person or virtually. I had always considered them as an incredible space, where I could be in contact with other developers and learn a lot. But nowadays, they have become a space to share with the community and for me to be able to help it grow."
+    },
+    {
+        number: "05.",
+        title: "Knowledge sharing",
+        text: "Throughout my life I have discovered that the best way to learn is to do it with the intention of sharing knowledge with others. Through sharing, true knowledge is reached. This has led me to be very active in the various development communities."
+    },
+    {
+        number: "06.",
+        title: "What moves me day by day?",
+        text: "It's an incredible feeling when people send you messages about how much they have benefited from what you have created, that they support you to keep going, that they like what you do or share. Those little moments are priceless. The fact of sharing what I know, of creating something that others can use, of being able to analyze my environment through data and improve my surroundings, those are the things that move me every day"
+    },
+];
