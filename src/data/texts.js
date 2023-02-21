88cf785a2b399875602d3fca8d5f9ec8f16696fb
Damian Sire <damiansire@hotmail.com>
2023-02-21 17:29:02 -0300

    feat: add text on js file

---
+++ src/data/texts.js
@@ -0,0 +1,3 @@
+export const texts = {
+    'yugioh-card-description-text': "Check out this amazing card! It has the ability to adapt to the current situation on the board, and can even turn it completely upside down!"
+}
