5250644125f0da8dd0b3db21dcc43360e888b7c4
Damian Sire <damiansire@hotmail.com>
2023-02-25 00:33:43 -0300

    fix: changed text Subscriptores to Subscribers

--- src/data/generalStats.js
+++ src/data/generalStats.js
@@ -1,5 +1,5 @@
 export const generalStats = [
-    { iconName: "mdi:youtube", title: "Subscriptores", text: "28.000" },
+    { iconName: "mdi:youtube", title: "Subscribers", text: "28.000" },
     { iconName: "majesticons:analytics-line", title: "Views on youtube", text: "1.300.000" },
     { iconName: "mdi:clock-time-four-outline", title: "Watch Time", text: "+70.000h" }
 ]
